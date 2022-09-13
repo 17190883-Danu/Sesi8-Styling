cd29c835af05899a2630638ce975d9c7588ab3aa
17190883-Danu <danutriprasetyo138@gmail.com>
2022-09-13 22:21:12 +0700

    Styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-// import './App.css';
+import React from 'react';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Header() {
   return (
@@ -21,8 +22,8 @@ function Header() {
         </div>
       </nav>
       <header className='py-3 mb-4 border-bottom'>
-        <div className='container f-flex flex-wrap justify-content-center'>
-          <a href='/d-flex align-center mb-3 mb-lg-0 me-lg-auto text dark 
+        <div className='container d-flex flex-wrap justify-content-center'>
+          <a href='/' className='d-flex align-center mb-3 mb-lg-0 me-lg-auto text dark 
           text-decoration-none '>
             <span className='fs-4'>My First React App</span>
           </a>
@@ -37,15 +38,38 @@ function Header() {
 
 function Content() {
   return (
+    <>
     <div className='p-5 mb-4 bg-light rounded-3 border'>
       <div className='container-fluid py-5'>
         <h1 className='display-5 fw-bold'>First Paragraph in Jumbotron</h1>
         <p className='col-md-8 fs-4'>Using a series of utilities, you can create this jumbroton, just like
         the one in previous version of Bootstrap. Check out the examples below for how you can remix and 
         restyle it to your liking.</p>
-        <button className='btn btn-primary btn-lg' type='button'>Example button</button>
+        <button className='btn btn-primary btn-lg' type='button' data-bs-toggle="modal" data-bs-target="#exampleModal">
+          Click here to show Modal box
+          </button>
       </div>
     </div>
+
+    {/* Modal */}
+    <div className='modal fade' id="exampleModal" tabindex="-1" aria-labelledby='exampleModalLabel' aria-hidden="true">
+      <div className='modal-dialog'>
+        <div className='modal-content'>
+          <div className='modal-header'>
+            <h5 className='modal-title' id="exampleModalLabel">Modal title</h5>
+            <button type='button' className='btn-close' data-bs-dismiss="modal" aria-label='Close'></button>
+          </div>
+          <div className='modal-body'>
+            This is my first modal box
+          </div>
+          <div className='modal-footer'>
+          <button type='button' className='btn btn-secondary' data-bs-dismiss="modal">Close</button>
+          <button type='button' className='btn btn-primary'>Save Changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
